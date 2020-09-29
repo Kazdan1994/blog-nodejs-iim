fa3e8953c62f2e11ac7c5aaff0bfe69303cde891
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-09-29 11:40:49 +0200

    feat(server): add show post

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs').__express;
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Bonjour tout le monde</h1>');
+    res.render('index');
+})
+app.get('/post', function (req, res) {
+    res.render('show')
 })
 
 app.use('/', express.static(path.resolve(__dirname, '..\\public')));
+
+app.engine('ejs', ejs);
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, function () {
     console.log('Server démarré sur http://localhost:3000');
